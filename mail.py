a4707491f52696ae3e96c4b9bbb044b391ffa269
0obstacleo0 <105998976+0obstacleo0@users.noreply.github.com>
2023-02-25 15:48:00 +0900

    Update mail.py

--- mail.py
+++ mail.py
@@ -5,15 +5,17 @@ from email.mime.text import MIMEText
 from email.utils import formatdate
 
 # 環境変数からパラメータ取得
-FROM_ADDRESS = os.environ["FROM_ADDRESS"]
+FROM_ADDRESS = os.environ['FROM_ADDRESS']
 TO_ADDRESS = os.environ["TO_ADDRESS"]
 PASSWORD = os.environ["PASSWORD"]
 
 # メール送信
 def send_mail(subject, text):
+    print("start")
     print(FROM_ADDRESS)
     print(TO_ADDRESS)
     print(PASSWORD)
+    print("end")
 #    smtpobj = smtplib.SMTP("smtp.gmail.com", 587)
 #    smtpobj.starttls()
 #    smtpobj.login(FROM_ADDRESS, PASSWORD)
